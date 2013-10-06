6216372dbcca844a58d52dfee4643f251ad57568
Monji Dolon <mdolon@gmail.com>
2013-10-06 15:25:17 -0400

    Adding deploy scripts

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,25 +1,37 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+server "198.199.76.102", :web, :app, :db, primary: true
 
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :application, "devgrow"
+set :user, "mdolon"
+set :deploy_to, "/home/#{user}/apps/#{application}"
+set :deploy_via, :remote_cache
+set :use_sudo, false
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+set :scm, "git"
+set :repository, "git@github.com:mdolon/devgrow.git"
+set :branch, "master"
 
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+set :bundle_dir, ''
+set :bundle_flags, '--system --quiet'
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+
+  task :setup_config, roles: :web do
+    sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
+    sudo "/etc/init.d/nginx reload"
+    sudo "service php5-fpm restart"
+    run "mkdir -p #{shared_path}/config"
+  end
+
+  desc "Make sure local git is in sync with remote."
+  task :check_revision, roles: :web do
+    unless `git rev-parse HEAD` == `git rev-parse origin/master`
+      puts "WARNING: HEAD is not the same as origin/master"
+      puts "Run `git push` to sync changes."
+      exit
+    end
+  end
+  before "deploy", "deploy:check_revision"
+end
