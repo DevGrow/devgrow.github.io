a8b0174cd6d253a727e9dc1d0799717298b26724
Monji Dolon <mdolon@gmail.com>
2013-10-17 17:16:24 -0400

    Fixing nav links

--- assets/_javascripts/application.coffee
+++ assets/_javascripts/application.coffee
@@ -3,9 +3,6 @@ $ ->
     trackEvent: (name, properties) =>
       mixpanel.track(name, properties)
 
-  mixpanel.track_links(".nav", "Clicked .nav link", {'referrer': document.referrer })
-  mixpanel.track_links(".social", "Clicked .social link", {'referrer': document.referrer })
-
   $("a[href^=\"http://\"],a[href^=\"https://\"]").not("[href*=\"devgrow.com\"]").click (e) ->
     url = @href
     window.App.trackEvent "Outbound Link",
@@ -16,3 +13,10 @@ $ ->
     url = @href
     window.App.trackEvent "Clicked post navigation link",
       target: url
+
+  $(".nav, .social").click (e) ->
+    url = @href
+    type = $(@).attr('class')
+    window.App.trackEvent "Clicked menu navigation",
+      target: url
+      type: type
--- assets/application.js
+++ assets/application.js
@@ -6,12 +6,6 @@
         return mixpanel.track(name, properties);
       }
     };
-    mixpanel.track_links(".nav", "Clicked .nav link", {
-      'referrer': document.referrer
-    });
-    mixpanel.track_links(".social", "Clicked .social link", {
-      'referrer': document.referrer
-    });
     $("a[href^=\"http://\"],a[href^=\"https://\"]").not("[href*=\"devgrow.com\"]").click(function(e) {
       var url;
       url = this.href;
@@ -20,11 +14,20 @@
         target: url
       });
     });
-    return $(".page-nav-item").click(function(e) {
+    $(".page-nav-item").click(function(e) {
       var url;
       url = this.href;
       return window.App.trackEvent("Clicked post navigation link", {
         target: url
+      });
+    });
+    return $(".nav, .social").click(function(e) {
+      var type, url;
+      url = this.href;
+      type = $(this).attr('class');
+      return window.App.trackEvent("Clicked menu navigation", {
+        target: url,
+        type: type
       });
     });
   });
